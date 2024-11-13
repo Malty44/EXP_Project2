9103bfbaff895a3ee9faf1d1250e62c47ec17cef
patrick4411 <patrick.magnani.4@gmail.com>
2024-11-13 15:32:04 +0100

    managed to make it work somehow, still have to do the MIME check

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'src', 'styles')));
+
+const mainRouter = require(path.join(__dirname, 'src', 'routes', 'router.js'));
+app.use('/', mainRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,24 +3,22 @@ const router = express.Router();
 
 // Root route
 router.get('/', (req, res) => {
-  res.send('Welcome to the application!');
+  const data = {
+    title: 'Home Page',
+    heading: 'Welcome to Our Website',
+    content: 'This is the content of the home page.',
+    products: [
+      { id: 1, name: 'Product 1', description: 'This is Product 1', price: 19.99 },
+      { id: 2, name: 'Product 2', description: 'This is Product 2', price: 24.99 },
+      { id: 3, name: 'Product 3', description: 'This is Product 3', price: 14.99 }
+    ],
+    year: new Date().getFullYear(),
+    company: 'My Company'
+  };
+
+  res.render('home', data);
 });
 
-// Example route
-router.get('/about', (req, res) => {
-  res.send('This is the about page.');
-});
 
-// Example route with parameter
-router.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`You requested user with ID: ${userId}`);
-});
-
-// Error handling
-router.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
 
 module.exports = router;
