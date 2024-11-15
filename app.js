29e54e02171b4598d4f7dc4b09faf56596656fcc
patrick4411 <patrick.magnani.4@gmail.com>
2024-11-15 19:52:34 +0100

    added dev run method

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'src', 'styles')));
-
+app.use(express.static('src'));
 const mainRouter = require(path.join(__dirname, 'src', 'routes', 'router.js'));
 app.use('/', mainRouter);
 
