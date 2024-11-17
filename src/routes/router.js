f20f6c7a6c803054eaa9b7fda312035df9e78a4b
patrick4411 <patrick.magnani.4@gmail.com>
2024-11-17 16:51:48 +0100

    progress on the main page

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,15 +5,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const data = {
     title: 'Home Page',
-    heading: 'Welcome to Our Website',
-    content: 'This is the content of the home page.',
-    products: [
-      { id: 1, name: 'Product 1', description: 'This is Product 1', price: 19.99 },
-      { id: 2, name: 'Product 2', description: 'This is Product 2', price: 24.99 },
-      { id: 3, name: 'Product 3', description: 'This is Product 3', price: 14.99 }
-    ],
-    year: new Date().getFullYear(),
-    company: 'My Company'
+    heading: 'Welcome to the experiment',
+    content: 'This is the page in which you can find all the information about the website',
+    
   };
 
   res.render('home', data);
